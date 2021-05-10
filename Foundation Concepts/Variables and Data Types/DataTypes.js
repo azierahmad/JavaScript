182f996160cfaf62c9a388fc905fd9952d6eaa73
Azier <a.mohammad44@protonmail.com>
2021-05-10 23:56:50 +0300

    Data Types and the most common ones in JavaScript are string, number, undefined, null, and Boolean.

---
+++ Foundation Concepts/Variables and Data Types/DataTypes.js
@@ -0,0 +1,28 @@
+//Data Types
+
+//1- String
+
+var myString = "My name is Azier!";
+var password = "password1234!";
+console.log(myString);
+
+
+// 2- Number
+
+var myNumber = 10;
+var myOtherNumber = 5;
+console.log(myNumber + myOtherNumber);
+
+
+//3- Undefined
+
+var myUndefined;
+console.log(myUndefined);
+
+//4- null
+// Null and undefined are sometimes incorrectly used interchangeably. They are very different! As you read above, undefined is the value that is given to a variable that has not yet explicitly been assigned a value. Null is a value that is explicitly assigned, and often used when wanting to explicitly state that nothing is currently being held in this variable.
+
+
+
+//5- Boolean
+// Boolean is a very fancy way of saying true or false!
