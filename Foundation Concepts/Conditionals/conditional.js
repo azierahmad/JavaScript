20406d4e962f2148614c4c0f3ed3c8e468ff883e
Azier <a.mohammad44@protonmail.com>
2021-05-11 00:33:24 +0300

    Conditionals - What if we have some code that we only want to execute under certain conditions? In computer programming, this is called a conditional statement.

---
+++ Foundation Concepts/Conditionals/conditional.js
@@ -0,0 +1,39 @@
+//if (condition) {    // what to do if condition is true
+//}
+//else if (2nd condition) {        // can have 0 to many of these statements    
+    // what to do if 2nd condition is true
+//}
+//else {                           // can have 0 or 1 of these statements    
+    // what to do if none of the previous conditions are met
+//}
+
+//Here are some examples:
+
+//1- If/Else Statements
+
+var x = 25;
+if (x > 50) {    
+    console.log("bigger than 50");
+}
+else {    
+    console.log("smaller than 50");
+}
+// because x is not greater than 50, the second print statement, "smaller than 50", is the only line that will execute
+
+//----------------------------------------------------------------
+
+var x = 75;
+if (x > 100) {    
+    console.log("bigger than 100");
+}
+else if (x > 50) {    
+    console.log("bigger than 50");
+}
+else if(x > 25) {
+    console.log("bigger than 25");
+}
+else {    
+    console.log("small number");
+}
+// because the first statement is not true, but the second is it will print "bigger than 50" to our console.  However, it won't even try to check our else if(x > 25) or else statements because it has already found something in the chain that is true.
+
