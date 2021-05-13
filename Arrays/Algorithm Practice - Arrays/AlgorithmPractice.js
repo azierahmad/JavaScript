76e2c032d37fe84fc80b7b85f06e18be015ca535
Azier <a.mohammad44@protonmail.com>
2021-05-13 09:10:34 +0300

    Algorithm Practice - Arrays

---
+++ Arrays/Algorithm Practice - Arrays/AlgorithmPractice.js
@@ -0,0 +1,6 @@
+var testArr = [6,3,5,1,2,4];
+var newArr = 0;
+for(var i = 0; i < testArr.length; i++){
+    newArr += testArr[i];
+    console.log("the index is: "+ i + " the num is: "+ newArr);
+}
