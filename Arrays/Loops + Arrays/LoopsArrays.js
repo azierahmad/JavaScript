b89687202c5dcb1b32bb9017afb61581cffebabd
Azier <a.mohammad44@protonmail.com>
2021-05-13 08:06:44 +0300

    Predict the Output: Arrays

---
+++ Arrays/Loops + Arrays/LoopsArrays.js
@@ -0,0 +1,7 @@
+//i will show in simple how can i use loops to can access into array and print its value >>>
+
+var data = ["ahmad", 15, "male"];
+for (var i = 0; i < data.length; i++){
+    console.log(i); //print the index
+    console.log(data[i]); // prints the array value at that index
+}
